ce541c3062afb6344f469382f0d70898cd889052
Gabriel Frattallone <gabrielfrattallone@gmail.com>
2018-03-03 00:43:21 -0400

    Added theme colors, about page, finished basic js events. No backend, no ajax, no schedule presentation

--- md_scheduler/scheduler/views.py
+++ md_scheduler/scheduler/views.py
@@ -6,7 +6,8 @@ def index(request):
     context = {
     	#TODO calculate year
     	#TODO add year to month name?
-    	#TODO differentiate May, June, July weeks w/ year
+    	#TODO differentiate May, June, July weeks w/ year?
+    	#TODO week dates?
     	"fall_year" : "2017",
     	"spring_year" : "2018",
     	"all_weeks" : ["A1", "A2", "A3", "A4", "S1", "S2", "S3", "S4", "O1", "O2", "O3", "O4", "N1", "N2", "N3", 
@@ -15,9 +16,13 @@ def index(request):
     	"row_1_months" : ["May", "June", "July", "August", "September", "October"],
     	"row_2_months" : ["November", "December", "January", "February", "March", "April"],
     	"row_3_months" : ["May", "June", "July"],
-    	#TODO fill these up:
+    	#TODO fill these up with actual valus:
     	"row_1_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
     	"row_2_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
     	"row_3_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
     }
     return render(request, 'scheduler/index.html', context)
+
+def about(request):
+	context = { }
+	return render(request, 'scheduler/about.html', context)
