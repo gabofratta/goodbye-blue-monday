0c7d96ef86c8d3440732d6a451c6f1acede01801
Gabo Frattallone <gabofratta@users.noreply.github.com>
2018-03-03 00:58:29 -0400

    Basic ui v1 (#1)
    * Basic ui for v1 first draft. Events still in progress. No backend
    
    * Added theme colors, about page, finished basic js events. No backend, no ajax, no schedule presentation

--- md_scheduler/scheduler/views.py
+++ md_scheduler/scheduler/views.py
@@ -3,5 +3,26 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context = {}
+    context = {
+    	#TODO calculate year
+    	#TODO add year to month name?
+    	#TODO differentiate May, June, July weeks w/ year?
+    	#TODO week dates?
+    	"fall_year" : "2017",
+    	"spring_year" : "2018",
+    	"all_weeks" : ["A1", "A2", "A3", "A4", "S1", "S2", "S3", "S4", "O1", "O2", "O3", "O4", "N1", "N2", "N3", 
+    					"N4", "D1", "D2", "D3", "D4", "E1", "E2", "E3", "E4", "F1", "F2", "F3", "F4", "M1", "M2", 
+						"M3", "M4", "B1", "B2", "B3", "B4"],
+    	"row_1_months" : ["May", "June", "July", "August", "September", "October"],
+    	"row_2_months" : ["November", "December", "January", "February", "March", "April"],
+    	"row_3_months" : ["May", "June", "July"],
+    	#TODO fill these up with actual valus:
+    	"row_1_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+    	"row_2_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+    	"row_3_activities" : [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+    }
     return render(request, 'scheduler/index.html', context)
+
+def about(request):
+	context = { }
+	return render(request, 'scheduler/about.html', context)
